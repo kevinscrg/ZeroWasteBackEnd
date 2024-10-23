baae68985ed09674a1d3163627040219d111d547
Darius Grigore Stoica <dariusgrigorestoica@gmail.com>
2024-10-23 18:37:05 +0300

    refactor products endpoint and included pycaches file in .gitignore

--- zerowaste/api/urls.py
+++ zerowaste/api/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('user/', UserDetailView.as_view(), name='user-detail'),
     path('logout/', LogoutView.as_view(), name="logout"),
-    path('products/', include('product.urls')),  
 ]
