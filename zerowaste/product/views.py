0a768cda768a15da719404ccb678ac4a1ec67241
kevinscrg <kevscrg@gmail.com>
2024-10-30 19:11:24 +0200

    Remove unused import for UserProductList in product views

--- zerowaste/product/views.py
+++ zerowaste/product/views.py
@@ -4,7 +4,6 @@ from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import UserProductList
 from .serializers import UserProductListSerializer
 from rest_framework.permissions import IsAuthenticated
 
