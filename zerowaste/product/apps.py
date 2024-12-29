f588bbf0c767d54fe046a6ba0a799f3873f9df71
kevinscrg <kevscrg@gmail.com>
2024-12-29 16:41:58 +0200

    Implement daily email scheduling for expiring products and update time zone setting

--- zerowaste/product/apps.py
+++ zerowaste/product/apps.py
@@ -1,6 +1,16 @@
+from datetime import date
+import sys
 from django.apps import AppConfig
 
 
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
+    
+    def ready(self):
+        if "zerowaste.asgi:application" in sys.argv:
+            from product.services.tasks import schedule_daily_emails  
+            today = date.today()
+            print(f"Scheduling daily emails for {today}...")
+            schedule_daily_emails.delay()
+            return super().ready()
